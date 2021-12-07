3560a5929078175cf0390132f4236f56ddca082b
robin vanelven <robin.vanelven@gmail.com>
2021-12-07 18:59:44 +0100

    maj fin de journée

--- api/simple.py
+++ api/simple.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 import json
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # pour lancer l'api
 # utiliser make run_api
